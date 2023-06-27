39e3a31ae45cc9494bd586a205e83e505b1d3fdc
jjackson9845 <137911030+jjackson9845@users.noreply.github.com>
2023-06-27 17:35:17 -0400

    Update streamlit_app.py
    Removed the display line of the json

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # Cleans up the output of the json and normalizes the formatting for display purposes 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
